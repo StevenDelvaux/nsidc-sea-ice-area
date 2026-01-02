103d4eb3774c4fda2d0cda11e6bef6b1cd0910d9
Steven Delvaux <steven.delvaux81@gmail.com>
2026-01-02 14:13:57 +0100

    Make calculations more robust against missing data

--- nsidc-regional-sia.py
+++ nsidc-regional-sia.py
@@ -614,7 +614,7 @@ def getSic(date, north = False):
 	f.close()
 	return sic
 
-def calculateArea(date, north=False, previousSic = None, twoDaysAgoSic = None, nextSic = None, nsidcGridCellAreas= None, regionalMask= None, validIceFlag= None):
+def calculateArea(date, north=False, previousSic = None, twoDaysAgoSic = None, threeDaysAgoSic = None, nextSic = None, nsidcGridCellAreas= None, regionalMask= None, validIceFlag= None):
 	"""
 	Calculate sea ice area for a daily gridded sea ice concentration file. 
     """
@@ -643,6 +643,9 @@ def calculateArea(date, north=False, previousSic = None, twoDaysAgoSic = None, n
 			if not cellconcentration >= 0 and valid == 1 and twoDaysAgoSic is not None :
 				#print('nan', cellconcentration, row, col)
 				cellconcentration = twoDaysAgoSic[row][col]
+			if not cellconcentration >= 0 and valid == 1 and threeDaysAgoSic is not None :
+				#print('nan', cellconcentration, row, col)
+				cellconcentration = threeDaysAgoSic[row][col]
 			if cellconcentration > 0 and valid == 1 and previousSic is not None and not previousSic[row][col] >= 0 and nextSic is not None and not nextSic[row][col] >= 0:
 				#print('nan', cellconcentration, row, col)
 				cellconcentration = 0			
@@ -1032,8 +1035,10 @@ def processAuto():
 	date = startdate
 	previousDay = date - timedelta(days = 1)
 	twoDaysAgo = date - timedelta(days = 2)
-	previousSic = getSic(date - timedelta(days = 1), north) if not previousDay in missingdates else None
+	threeDaysAgo = date - timedelta(days = 3)
+	previousSic = getSic(previousDay, north) if not previousDay in missingdates else None
 	twoDaysAgoSic = getSic(twoDaysAgo, north) if not twoDaysAgo in missingdates else None
+	threeDaysAgoSic = getSic(threeDaysAgo, north) if not threeDaysAgo in missingdates else None
 
 	#enddate = yesterday if auto else datetime(2023,12,18)
 	nextSic = None
@@ -1048,7 +1053,8 @@ def processAuto():
 			validIceFlag = np.loadtxt(open("masks/valid_ice_flag_" + padzeros(date.month) + ".csv", "rb"), delimiter=",", skiprows=0)		
 			print('inside process auto bis', north, hemisphere)
 			if(date.month != 2 or date.day != 29): # skip leap days
-				currentSic = calculateArea(date, north, previousSic, twoDaysAgoSic, nextSic, nsidcGridCellAreas, regionalMask, validIceFlag)
+				currentSic = calculateArea(date, north, previousSic, twoDaysAgoSic, threeDaysAgoSic, nextSic, nsidcGridCellAreas, regionalMask, validIceFlag)
+				threeDaysAgoSic = twoDaysAgoSic
 				twoDaysAgoSic = previousSic
 				previousSic = currentSic
 		except:
